be849b8799bc756e7af02169e2cbf006506bfdc2
Luis Javier Paz C <lujapak@gmail.com>
2023-09-12 07:58:14 -0500

    perf:add slint and prettier

--- social-stream-frontend/src/App.tsx
+++ social-stream-frontend/src/App.tsx
@@ -3,18 +3,19 @@ import {
   PopoverHandler,
   PopoverContent,
   Button,
-} from "@material-tailwind/react";
-const App = () => {
-  return ( 
-      <Popover>
-        <PopoverHandler>
-          <Button>Show Popover</Button>
-        </PopoverHandler>
-        <PopoverContent>
-          This is a very beautiful popover, show some love.
-        </PopoverContent>
-      </Popover>
-    );
+} from '@material-tailwind/react';
+import type React from 'react';
+const App: React.FC = () => {
+  return (
+    <Popover>
+      <PopoverHandler>
+        <Button>Show Popover</Button>
+      </PopoverHandler>
+      <PopoverContent>
+        This is a very beautiful popover, show some love.
+      </PopoverContent>
+    </Popover>
+  );
 };
 
 export default App;
--- social-stream-frontend/src/main.tsx
+++ social-stream-frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const documentRoot = document.getElementById('root');
+if (documentRoot !== null && documentRoot !== undefined) {
+  ReactDOM.createRoot(documentRoot).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
